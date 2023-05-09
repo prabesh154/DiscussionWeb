981979765c87e8be6a6ea3bd96217cee6755acc4
prabesh <prabeshbizz@gmail.com>
2023-05-09 23:07:38 +0545

    Models attributes and rendering in templates

--- room/views.py
+++ room/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
+from .models import Room
 
 
-
-rooms = [
-    {'id': 1, 'name':'Lets Code'},
-    {'id': 2, 'name':'Lets Code'},
-    {'id': 3, 'name':'Lets Code'},
-]
+# rooms = [
+##  {'id': 1, 'name':'Lets Code'},
+## {'id': 2, 'name':'Lets Code'},
+# {'id': 3, 'name':'Lets Code'},
+#]
     
 
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     context={'rooms': rooms}
     return render(request, 'home.html',context)
 
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+    #for i in rooms:
+     #   if i['id'] == int(pk):
+            #room = i
     context={'room': room}
     return render(request, 'room.html',context)
